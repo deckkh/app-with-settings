baf30e22939c773c492104f90e2311f634f1f47b
Kim Kruse Hansen <deckkh@hotmail.com>
2021-08-16 18:09:15 +0200

    load properties from file

---
+++ app/src/main/java/app/with/settings/MyProperties.java
@@ -0,0 +1,36 @@
+package app.with.settings;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MyProperties {
+	String result = "";
+	InputStream inputStream;
+ 
+	public Properties getPropValues() throws IOException 
+    {
+ 
+        Properties prop = new Properties();
+		try {
+			String propFileName = "config.properties";
+ 
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+ 
+ 
+			// get the property value and print it out
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+		return prop;
+	}
+}
